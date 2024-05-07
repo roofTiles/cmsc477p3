ef74d8044169a74efaf70874e98093e60b413bd1
roofTiles <guhatejas@gmail.com>
2024-05-07 12:31:23 -0400

    ir sensor + obstacle detection with cam
    - irdistspannerpy is movement based solely
    on infrared + encoder to avoid obstacles
    - detection.py use new obstacles detection
    vision model

--- detection.py
+++ detection.py
@@ -4,10 +4,10 @@ from robomaster import camera
 import cv2
 import numpy as np
 
-model = YOLO("models/best.pt")
+model = YOLO("models/proj3.pt")
 
 # c is the desired class type for obj detection with the following
-# options 'robot' and 'lego'
+# options 'obstacle' and 'robot'
 
 # conf is the desired confidence interval for detection
 # image to be used for obj detection. If none given then
@@ -15,99 +15,78 @@ model = YOLO("models/best.pt")
 
 # returns the bounding box array of format [x,y,w,h]
 # normalized to original image size
-def detect_object_in_image(c='robot', conf=0.5, image=None, ep_camera=None):
+def detect_object_in_image(c='obstacle', conf=0.5, image=None, ep_camera=None):
 
     if image == None:
         image = ep_camera.read_cv2_image(strategy="newest", timeout=5)
 
+    print(image.shape)
 
     classes = []
 
     if c == 'both':
         classes = [0,1]
-
-    if c == 'lego':
-        classes=[0]
     if c == 'robot':
+        classes=[0]
+    if c == 'obstacle':
         classes = [1]
         
     results = model.predict(source=image, show=False, conf = conf,
                             imgsz=(384, 640), classes=classes)
-    
-    if len(results) > 1:
-        raise Exception("More than one lego detected!")
 
-    if len(results) == 1: # only one detection found
-        result = results[0].cpu().numpy()
-        boxes = result.boxes.xywhn
-
-        if len(boxes) == 0: #nothing found
+    if len(results) == 1: # no detections
+        boxes = results[0].cpu().numpy().boxes.xywhn
+        if len(boxes) == 0:
             return [False, None]
 
-        bb = boxes[0]
+    biggest_height_index = 0
+    biggest_height = 0
+    for i, r in enumerate(results):
+        box = (r.cpu().numpy().boxes.xywhn)[0]
+        height = box[3]
+        if height > biggest_height:
+            biggest_height = height
+            biggest_height_index = i
 
-        # scales to input pixel coords
-        bb[0] = bb[0]*640
-        bb[1] = bb[1]*384
-        bb[2] = bb[2]*640
-        bb[3] = bb[3]*384
-        return [True, boxes[0]]
-
-# this function returns the bounding box of the endpoint
-# in the image. color parameter indicates what color
-# endpoint to look for (yellow or red)
-# returns [x,y,w,h]
-def detect_endpoint(image=None, color="yellow", show=True, ep_camera=None):
-
-    if image == None:
-        image = ep_camera.read_cv2_image(strategy="newest", timeout=5)
-
-    if show:
-        original = image
-
-    # masking all other colors except yellow
-    if color=="yellow":
-        mask = cv2.inRange(image, np.asarray([0, 200, 200]), np.asarray([150, 255, 255])) #bgr channel order
-        image = cv2.bitwise_and(image, image, mask = mask)
-
-    # performing erosion to erase false positives
-    kernel = np.ones((2, 2), np.uint8)
-    image = cv2.erode(image, kernel, iterations=5)
-
-    # convert image to gray scale
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # applying otsu binarization
-    thres, image = cv2.threshold(image, 0, 255,
-                          cv2.THRESH_OTSU)
+    closest_obstacle = results[biggest_height_index]
+    bb = (closest_obstacle.cpu().numpy().boxes.xywhn)[0]
     
-    # get contours of image
-    cnts, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = sorted(cnts, key=cv2.contourArea, reverse=True) # sort by area
+    # scales to input pixel coords
+    bb[0] = bb[0]*1280
+    bb[1] = bb[1]*720
+    bb[2] = bb[2]*1280
+    bb[3] = bb[3]*720
 
-    if len(cnt) == 0: # no endpoint found
-        return [False, None]
+    corners = []
+    corners.append([bb[0] - bb[2]/2, bb[1] - bb[3]/2])
+    corners.append([bb[0] - bb[2]/2, bb[1] + bb[3]/2])
+    corners.append([bb[0] + bb[2]/2, bb[1] + bb[3]/2])
+    corners.append([bb[0] + bb[2]/2, bb[1] - bb[3]/2])
+    corners = np.asarray(corners)
+
+    pts = corners.reshape((-1, 1, 2)).astype(np.int32)
+    image = cv2.polylines(image, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
+    cv2.imshow("img", image)
+    cv2.waitKey(10)
     
-    endpoint_cnt = cnt[0]
+    #            cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
     
-    # get bb around endpoint contour
-    x,y,w,h = cv2.boundingRect(endpoint_cnt)
+    return [True, bb]
 
-    if w < 25:
-        return [False, None]
 
-    # show where endpoint being detected
-    if show:
-        original = cv2.rectangle(original,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.imshow('Location of endpoint', original)
-        cv2.waitKey(0)
-
-    return [True, [x, y, w, h]]
-    
 if __name__ == '__main__':
-    img = cv2.imread("C:\\umd\\senior\\cmsc477\\Project 2\\proj_code\\endpoint_test\\6.jpg")
-    img = cv2.resize(img, (640, 384))
-    detect_endpoint(image=img)
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+    ep_chassis = ep_robot.chassis
+    ep_camera = ep_robot.camera
+    ep_gripper = ep_robot.gripper
+    ep_arm = ep_robot.robotic_arm
+
+    #ep_camera.start_video_stream(display=True)
+
+    while True:
+        print(detect_object_in_image(c='both', ep_camera=ep_camera))
+    
             
             
     
